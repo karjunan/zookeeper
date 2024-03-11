bf32159225b49c3363ddcf587f6061438dca2ec4
krishna <arjunankrishnakumar1@gmail.com>
2024-03-11 23:37:46 +0530

    monitor server

---
+++ zookeeper-demos/src/main/java/com/example/zookeeperdemos/Monitor.java
@@ -0,0 +1,31 @@
+package com.example.zookeeperdemos;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Monitor {
+
+    public static ZooKeeper zooKeeper;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        zooKeeper = new ZooKeeper("localhost:2181", 15000, (watcher) -> {
+
+            try {
+                List<String> childern = zooKeeper.getChildren("/",true, null);
+                for (String v  : childern) {
+                    System.out.println(v +" ");
+                }
+                System.out.println(" ");
+            } catch (KeeperException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        Thread.sleep(100_100_100);
+    }
+}
---
+++ zookeeper-demos/src/main/java/com/example/zookeeperdemos/Server3.java
@@ -0,0 +1,27 @@
+package com.example.zookeeperdemos;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static org.apache.zookeeper.ZooDefs.Ids.OPEN_ACL_UNSAFE;
+
+@SpringBootApplication
+public class Server3 {
+
+	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+//		SpringApplication.run(ZookeeperDemosApplication.class, args);
+
+		String id = UUID.randomUUID().toString();
+		System.out.println("ID");
+		ZooKeeper zooKeeper = new ZooKeeper("localhost:2181",15000,null);
+		var createResponse = zooKeeper.create("/server-"+id,id.getBytes(), OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, null);
+		System.out.println(createResponse);
+		Thread.sleep(100_000_000);
+	}
+
+}
