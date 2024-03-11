0cde5358c2353191b87cffc25a6abe568e74d044
krishna <arjunankrishnakumar1@gmail.com>
2024-03-11 23:38:57 +0530

    monitor server

---
+++ zookeeper-demos/src/test/java/com/example/zookeeperdemos/ZookeeperDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zookeeperdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
